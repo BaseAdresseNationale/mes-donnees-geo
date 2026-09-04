03060c390a63c718b93e97e8756c92edd1a8061c
Guillaume Fay <guillaume_fay@hotmail.com>
2026-09-04 16:22:16 +0200

    fix: allow any search param in next config

--- next.config.mjs
+++ next.config.mjs
@@ -8,10 +8,20 @@ const nextConfig = {
   ],
   images: {
     remotePatterns: [
-      new URL(`${process.env.NEXT_PUBLIC_PANORAMAX_API_URL}/**`),
-      new URL(
-        `${process.env.NEXT_PUBLIC_BUCKET_ANNUAIRE_DES_COLLECTIVITES}/**`,
-      ),
+      {
+        protocol: "https",
+        hostname: new URL(process.env.NEXT_PUBLIC_PANORAMAX_API_URL).hostname,
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: new URL(
+          process.env.NEXT_PUBLIC_BUCKET_ANNUAIRE_DES_COLLECTIVITES,
+        ).hostname,
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 };
